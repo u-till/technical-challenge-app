335ac21e4c0ccfb92db158e10a39d7eaecb641ef
utill <tillsolenthaler@google.com>
2020-07-13 11:49:10 +0200

    fixed spelling in constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,5 @@
 const baseUrl = "http://localhost:8000/backend/api/"; ////
-// const baseUrl = 'https://tech-challange.propulsion-learn.ch/backend/api/';//
+// const baseUrl = 'https://tech-challenge.propulsion-learn.ch/backend/api/';//
 
-export const defaultUserProfileBanner = "https://via.placeholder.com/1200x300";
 
 export default baseUrl;
