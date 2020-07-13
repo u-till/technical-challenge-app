a789c90b3da9ee8506c7f5af57a43d057a585e15
utill <tillsolenthaler@google.com>
2020-07-13 11:39:34 +0200

    fixed spelling in backend

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,4 +4,4 @@ rm -rf /frontend/build/* && cp -r /frontend_tmp/build/* /frontend/build
 python manage.py collectstatic --no-input
 python manage.py makemigrations --no-input
 python manage.py migrate
-gunicorn -w 4 -b 0.0.0.0:8000 luna_project.wsgi:application
+gunicorn -w 4 -b 0.0.0.0:8000 techa_project.wsgi:application
