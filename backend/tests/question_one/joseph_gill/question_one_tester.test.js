dd6516d5c53c0f00aedc32fa81393770e5137f02
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-07-17 16:31:05 +0200

    Adding test functionality

---
+++ backend/tests/question_one/joseph_gill/question_one_tester.test.js
@@ -0,0 +1,24 @@
+const test = require('tape');
+
+function sum(x, y) {return x + y};
+
+test('function should return the sum of two numbers', function(t) {
+    const result = sum(1,2); 
+    const expected = 3;
+    t.deepEqual(result, expected);
+    t.end();
+    });
+
+test('function should return the sum of two numbers', function(t) {
+     const result = sum(4,10);
+     const expected = 14;
+     t.deepEqual(result, expected);
+     t.end();
+     });
+
+test('function should return the sum of two numbers', function(t) {
+     const result = sum(10,-1);
+     const expected = 9;
+     t.deepEqual(result, expected);
+     t.end();
+     });
--- backend/tests/urls.py
+++ backend/tests/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from tests.views import RunTestOne
+from tests.views import *
 
 urlpatterns = [
     path('1/', RunTestOne.as_view()),
+    path('2/', RunTestTwo.as_view()),
+    path('3/', RunTestThree.as_view()),
+    path('4/', RunTestFour.as_view()),
+    path('5/', RunTestFive.as_view()),
+    path('6/', RunTestSix.as_view()),
+    path('7/', RunTestSeven.as_view()),
+    path('8/', RunTestEight.as_view()),
+    path('9/', RunTestNine.as_view()),
+    path('10/', RunTestTen.as_view()),
 ]
