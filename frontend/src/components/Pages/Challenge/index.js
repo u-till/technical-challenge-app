fca0c4302dde8f4342506beba34ce5c62b139bbf
utill <tillsolenthaler@google.com>
2020-07-21 10:11:12 +0200

    added addusermodal

--- frontend/src/components/Pages/Challenge/index.js
+++ frontend/src/components/Pages/Challenge/index.js
@@ -1,22 +1,22 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import {rem} from "polished";
-import {BaseContainer, PageContainer} from "../../../style/GlobalWrappers";
-import {Styledh1, Styledh2} from "../../../style/GlobalTitles";
-import {BlueButton, RedButton} from "../../../style/GlobalButtons";
+import { rem } from "polished";
+import { BaseContainer, PageContainer } from "../../../style/GlobalWrappers";
+import { Styledh1, Styledh2 } from "../../../style/GlobalTitles";
+import { BlueButton, RedButton } from "../../../style/GlobalButtons";
 import {
-    Container as ResizeContainer,
-    Section,
-    Bar,
+  Container as ResizeContainer,
+  Section,
+  Bar,
 } from "react-simple-resizer";
-import {connect, useDispatch} from "react-redux";
-import {getChallenge} from "../../../store/actions/challengeAction";
-import {Controlled as CodeMirror} from 'react-codemirror2'
-import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/material.css';
-import 'codemirror/mode/javascript/javascript.js';
-import {getUserChallengeAction} from "../../../store/actions/challengeActions";
-import {useRouteMatch} from "react-router-dom";
+import { connect, useDispatch } from "react-redux";
+import { getChallenge } from "../../../store/actions/challengeAction";
+import { Controlled as CodeMirror } from "react-codemirror2";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/javascript/javascript.js";
+import { getUserChallengeAction } from "../../../store/actions/challengeActions";
+import { useRouteMatch } from "react-router-dom";
 
 //////////
 // STYLE
@@ -186,192 +186,220 @@ const DoneButton = styled(RedButton)``;
 // REACT
 //////////
 
-const Challenge = ({targetChallenge, getUserChallengeAction}) => {
-    const dispatch = useDispatch();
-    const match = useRouteMatch();
+const Challenge = ({ targetChallenge, getUserChallengeAction }) => {
+  const dispatch = useDispatch();
+  const match = useRouteMatch();
 
-    const [progressValue, setProgressValue] = useState(0);
-    const [initDate, getInitDate] = useState(0);
-    const [middleCodeMirror, setMiddleCodeMirror] = useState('');
-    const [rightCodeMirror, setRightCodeMirror] = useState('');
+  const [progressValue, setProgressValue] = useState(0);
+  const [initDate, getInitDate] = useState(0);
+  const [middleCodeMirror, setMiddleCodeMirror] = useState("");
+  const [rightCodeMirror, setRightCodeMirror] = useState("");
 
-    const timerComponents = [];
+  const timerComponents = [];
 
-    const calculateTimeLeft = () => {
-        const dateNow = new Date();
-        const databaseDate = new Date(String(initDate))
-        let difference = dateNow - databaseDate;
-        difference = 1800000 - difference
-        if (difference > 0) {
-            let timeLeft = `Time left: ${Math.floor((difference / 1000 / 60) % 60)}:${Math.floor((difference / 1000) % 60)}`;
-            return timeLeft;
-        }
-    };
-
-    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
-
-    useEffect(() => {
-        const startChallenge = async () => {
-            const initDate = await dispatch(getChallenge());
-            getInitDate(initDate);
-        };
-
-        startChallenge();
-
-        const settimeout = setTimeout(() => {
-            setTimeLeft(calculateTimeLeft());
-        }, 1000);
-
-        return () => clearInterval(settimeout)
-    });
-
-    useEffect(() => {
-        getUserChallengeAction(match.params.challengeId);
-    }, [getUserChallengeAction]);
-
-    const options = {
-        mode: 'javascript',
-        theme: 'material',
-        lineNumbers: true,
-    };
-
-    const renderControlPanel = (progressValue) => {
-        if (progressValue === 0) {
-            return (<>
-                    <PrevNextButtonDisabled>Previous</PrevNextButtonDisabled>
-                    <StepSelectorLine>
-                        <StepSelectorContainer>
-                            <StepSelectorBtnActive/>
-                            <StepSelectorBtn/>
-                            <StepSelectorBtn/>
-                            <StepSelectorBtn/>
-                            <StepSelectorBtn/>
-                        </StepSelectorContainer>
-                    </StepSelectorLine>
-                    <PrevNextButton onClick={e => setProgressValue(1)}>Next</PrevNextButton> </>
-            );
-        }
-        if (progressValue === 1) {
-            return (<>
-                    <PrevNextButton onClick={e => setProgressValue(0)}>Previous</PrevNextButton>
-                    <StepSelectorLine>
-                        <StepSelectorContainer>
-                            <StepSelectorBtnActive/>
-                            <StepSelectorBtnActive/>
-                            <StepSelectorBtn/>
-                            <StepSelectorBtn/>
-                            <StepSelectorBtn/>
-                        </StepSelectorContainer>
-                    </StepSelectorLine>
-                    <PrevNextButton onClick={e => setProgressValue(2)}>Next</PrevNextButton> </>
-            )
-        }
-        if (progressValue === 2) {
-            return (<>
-                    <PrevNextButton onClick={e => setProgressValue(1)}>Previous</PrevNextButton>
-                    <StepSelectorLine>
-                        <StepSelectorContainer>
-                            <StepSelectorBtnActive/>
-                            <StepSelectorBtnActive/>
-                            <StepSelectorBtnActive/>
-                            <StepSelectorBtn/>
-                            <StepSelectorBtn/>
-                        </StepSelectorContainer>
-                    </StepSelectorLine>
-                    <PrevNextButton onClick={e => setProgressValue(3)}>Next</PrevNextButton> </>
-            )
-        }
-        if (progressValue === 3) {
-            return (<>
-                    <PrevNextButton onClick={e => setProgressValue(2)}>Previous</PrevNextButton>
-                    <StepSelectorLine>
-                        <StepSelectorContainer>
-                            <StepSelectorBtnActive/>
-                            <StepSelectorBtnActive/>
-                            <StepSelectorBtnActive/>
-                            <StepSelectorBtnActive/>
-                            <StepSelectorBtn/>
-                        </StepSelectorContainer>
-                    </StepSelectorLine>
-                    <PrevNextButton onClick={e => setProgressValue(4)}>Next</PrevNextButton> </>
-            )
-        }
-        return (
-            <>
-                <PrevNextButton onClick={(e) => setProgressValue(3)}>
-                    Previous
-                </PrevNextButton>
-                <StepSelectorLine>
-                    <StepSelectorContainer>
-                        <StepSelectorBtnActive/>
-                        <StepSelectorBtnActive/>
-                        <StepSelectorBtnActive/>
-                        <StepSelectorBtnActive/>
-                        <StepSelectorBtnActive/>
-                    </StepSelectorContainer>
-                </StepSelectorLine>
-                <PrevNextButtonDisabled>Next</PrevNextButtonDisabled>
-                <div/>
-            </>
-        );
-    };
-
-    return (
-        <>
-            <ChallengeContainer>
-                <StyledResizeContainer>
-                    <DescriptionColumn>
-                        {targetChallenge ? (
-                            <DescriptionContainer>
-                                <DescriptionHeader>
-                                    <div>
-                                        <Styledh1>{targetChallenge.questions[progressValue].name}</Styledh1>
-                                    </div>
-                                </DescriptionHeader>
-                                <div>
-                                    <p>{targetChallenge.questions[progressValue].instructions}</p>
-                                </div>
-                            </DescriptionContainer>) : null}
-                    </DescriptionColumn>
-                    <StyledResizeBar/>
-                    <InputColumn>
-                        <CodeMirror
-                            value={middleCodeMirror}
-                            options={options}
-                            onBeforeChange={(editor, data, value) => {
-                                setMiddleCodeMirror(value)
-                            }}
-                            onChange={(editor, data, value) => {
-                            }}
-                        />
-                    </InputColumn>
-                    <StyledResizeBar/>
-                    <OutputColumn>
-                        {targetChallenge ? targetChallenge.questions[progressValue].tests_for_question.map(test =>
-                            <p>{test}</p>) : null}
-
-                    </OutputColumn>
-                </StyledResizeContainer>
-            </ChallengeContainer>
-            <Footer>
-                <FooterSectionLeft>
-                    {renderControlPanel(progressValue)}
-                </FooterSectionLeft>
-                <FooterSectionRight>
-                    <Timer>
-                        <p>Time left: 24:05</p>
-                    </Timer>
-                    <DoneButton>Done!</DoneButton>
-                </FooterSectionRight>
-            </Footer>
-        </>
-    );
-};
-
-const mapStateToProps = state => {
-    return {
-        targetChallenge: state.challengeReducer.targetChallenge
+  const calculateTimeLeft = () => {
+    const dateNow = new Date();
+    const databaseDate = new Date(String(initDate));
+    let difference = dateNow - databaseDate;
+    difference = 1800000 - difference;
+    if (difference > 0) {
+      let timeLeft = `Time left: ${Math.floor(
+        (difference / 1000 / 60) % 60
+      )}:${Math.floor((difference / 1000) % 60)}`;
+      return timeLeft;
     }
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const startChallenge = async () => {
+      const initDate = await dispatch(getChallenge());
+      getInitDate(initDate);
+    };
+
+    startChallenge();
+
+    const settimeout = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearInterval(settimeout);
+  });
+
+  useEffect(() => {
+    getUserChallengeAction(match.params.challengeId);
+  }, [getUserChallengeAction]);
+
+  const options = {
+    mode: "javascript",
+    theme: "material",
+    lineNumbers: true,
+  };
+
+  const renderControlPanel = (progressValue) => {
+    if (progressValue === 0) {
+      return (
+        <>
+          <PrevNextButtonDisabled>Previous</PrevNextButtonDisabled>
+          <StepSelectorLine>
+            <StepSelectorContainer>
+              <StepSelectorBtnActive />
+              <StepSelectorBtn />
+              <StepSelectorBtn />
+              <StepSelectorBtn />
+              <StepSelectorBtn />
+            </StepSelectorContainer>
+          </StepSelectorLine>
+          <PrevNextButton onClick={(e) => setProgressValue(1)}>
+            Next
+          </PrevNextButton>{" "}
+        </>
+      );
+    }
+    if (progressValue === 1) {
+      return (
+        <>
+          <PrevNextButton onClick={(e) => setProgressValue(0)}>
+            Previous
+          </PrevNextButton>
+          <StepSelectorLine>
+            <StepSelectorContainer>
+              <StepSelectorBtnActive />
+              <StepSelectorBtnActive />
+              <StepSelectorBtn />
+              <StepSelectorBtn />
+              <StepSelectorBtn />
+            </StepSelectorContainer>
+          </StepSelectorLine>
+          <PrevNextButton onClick={(e) => setProgressValue(2)}>
+            Next
+          </PrevNextButton>{" "}
+        </>
+      );
+    }
+    if (progressValue === 2) {
+      return (
+        <>
+          <PrevNextButton onClick={(e) => setProgressValue(1)}>
+            Previous
+          </PrevNextButton>
+          <StepSelectorLine>
+            <StepSelectorContainer>
+              <StepSelectorBtnActive />
+              <StepSelectorBtnActive />
+              <StepSelectorBtnActive />
+              <StepSelectorBtn />
+              <StepSelectorBtn />
+            </StepSelectorContainer>
+          </StepSelectorLine>
+          <PrevNextButton onClick={(e) => setProgressValue(3)}>
+            Next
+          </PrevNextButton>{" "}
+        </>
+      );
+    }
+    if (progressValue === 3) {
+      return (
+        <>
+          <PrevNextButton onClick={(e) => setProgressValue(2)}>
+            Previous
+          </PrevNextButton>
+          <StepSelectorLine>
+            <StepSelectorContainer>
+              <StepSelectorBtnActive />
+              <StepSelectorBtnActive />
+              <StepSelectorBtnActive />
+              <StepSelectorBtnActive />
+              <StepSelectorBtn />
+            </StepSelectorContainer>
+          </StepSelectorLine>
+          <PrevNextButton onClick={(e) => setProgressValue(4)}>
+            Next
+          </PrevNextButton>{" "}
+        </>
+      );
+    }
+    return (
+      <>
+        <PrevNextButton onClick={(e) => setProgressValue(3)}>
+          Previous
+        </PrevNextButton>
+        <StepSelectorLine>
+          <StepSelectorContainer>
+            <StepSelectorBtnActive />
+            <StepSelectorBtnActive />
+            <StepSelectorBtnActive />
+            <StepSelectorBtnActive />
+            <StepSelectorBtnActive />
+          </StepSelectorContainer>
+        </StepSelectorLine>
+        <PrevNextButtonDisabled>Next</PrevNextButtonDisabled>
+        <div />
+      </>
+    );
+  };
+
+  return (
+    <>
+      <ChallengeContainer>
+        <StyledResizeContainer>
+          <DescriptionColumn>
+            {targetChallenge ? (
+              <DescriptionContainer>
+                <DescriptionHeader>
+                  <div>
+                    <Styledh1>
+                      {targetChallenge.questions[progressValue].name}
+                    </Styledh1>
+                  </div>
+                </DescriptionHeader>
+                <div>
+                  <p>{targetChallenge.questions[progressValue].instructions}</p>
+                </div>
+              </DescriptionContainer>
+            ) : null}
+          </DescriptionColumn>
+          <StyledResizeBar />
+          <InputColumn>
+            <CodeMirror
+              value={middleCodeMirror}
+              options={options}
+              onBeforeChange={(editor, data, value) => {
+                setMiddleCodeMirror(value);
+              }}
+              onChange={(editor, data, value) => {}}
+            />
+          </InputColumn>
+          <StyledResizeBar />
+          <OutputColumn>
+            {targetChallenge
+              ? targetChallenge.questions[
+                  progressValue
+                ].tests_for_question.map((test) => <p>{test}</p>)
+              : null}
+          </OutputColumn>
+        </StyledResizeContainer>
+      </ChallengeContainer>
+      <Footer>
+        <FooterSectionLeft>
+          {renderControlPanel(progressValue)}
+        </FooterSectionLeft>
+        <FooterSectionRight>
+          <Timer>
+            <p>Time left: 24:05</p>
+          </Timer>
+          <DoneButton>Done!</DoneButton>
+        </FooterSectionRight>
+      </Footer>
+    </>
+  );
 };
 
-export default connect(mapStateToProps, {getUserChallengeAction})(Challenge);
+const mapStateToProps = (state) => {
+  return {
+    targetChallenge: state.challengeReducer.targetChallenge,
+  };
+};
+
+export default connect(mapStateToProps, { getUserChallengeAction })(Challenge);
--- frontend/src/store/actions/challengeActions.js
+++ frontend/src/store/actions/challengeActions.js
@@ -1,37 +1,40 @@
 import Axios from "../../axios";
-import {GET_ALL_CHALLENGES_FOR_USER, GET_USERS_CHALLENGE} from '../actionTypes';
+import {
+  GET_ALL_CHALLENGES_FOR_USER,
+  GET_USERS_CHALLENGE,
+} from "../actionTypes";
 
 export const getAllUserChallenges = (challenges) => {
-      return {
-          type: GET_ALL_CHALLENGES_FOR_USER,
-          payload: challenges
-      }
+  return {
+    type: GET_ALL_CHALLENGES_FOR_USER,
+    payload: challenges,
+  };
 };
 
 export const getUserChallenge = (challenge) => {
-    return {
-        type: GET_USERS_CHALLENGE,
-        payload: challenge
-    }
+  return {
+    type: GET_USERS_CHALLENGE,
+    payload: challenge,
+  };
 };
 
 export const getAllUserChallengesAction = () => async (dispatch) => {
-    try {
-        const response = await Axios.get('challenges/me/');
-        dispatch(getAllUserChallenges(response.data));
-        return response
-    } catch (error) {
-        console.log('Error getting all User Challenges>', error)
-    }
+  try {
+    const response = await Axios.get("challenges/me/");
+    dispatch(getAllUserChallenges(response.data));
+    return response;
+  } catch (error) {
+    console.log("Error getting all User Challenges>", error);
+  }
 };
 
 export const getUserChallengeAction = (challengeId) => async (dispatch) => {
-    try {
-        const response = await Axios.get(`challenges/edit/${challengeId}/`);
-        dispatch(getUserChallenge(response.data));
-        return response
-    } catch (error) {
-        console.log('Error getting a specific User Challenge>', error);
-        return error
-    }
+  try {
+    const response = await Axios.get(`challenges/edit/${challengeId}/`);
+    dispatch(getUserChallenge(response.data));
+    return response;
+  } catch (error) {
+    console.log("Error getting a specific User Challenge>", error);
+    return error;
+  }
 };
