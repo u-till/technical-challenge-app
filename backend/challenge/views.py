87a6b95c600f54ca2384196f991f45ebb390a6e0
Natayra Pacheco dos Santos <natayras@gmail.com>
2020-07-22 11:39:09 +0200

    Email sending end challenge

--- backend/challenge/views.py
+++ backend/challenge/views.py
@@ -142,7 +142,61 @@ class ChallengeScore(UpdateAPIView):
     def partial_update(self, request, *args, **kwargs):
         score = request.data['score']
         challenge = Challenge.objects.get(id=kwargs['id'])
+        candidate = User.objects.get(id=challenge.candidate_id)
+        creator = User.objects.get(id=challenge.creator_id)
         if challenge.score is None:
             challenge.score = score
             challenge.save()
+            email = EmailMultiAlternatives()
+            email.subject = f'Propulsion Academy - Challenge Results'
+            email.to = [candidate.email]
+            if challenge.score < 40:
+                result = 'Failed'
+                html_content = f"""<h3 style="font-weight:normal">Unfortunately you didn't made it.</h3>
+                        <h3 style="font-weight:normal">Better luck next time</h3>
+                        <p><strong>Propulsion Academy</strong><br>
+                        Technoparkstrasse 1<br>
+                        8005 Zürich, Switzerland<br>
+                        https://propulsion.academy/full-stack</p>
+                        <img src="https://ci6.googleusercontent.com/proxy/D1srIpj53axfX_D5ZAZRlbc5aW_wo_qIcq9U0HynZroJDhCh-sS_cobQ8ulokzLaAm29-KHvII6JPVqy3tkJueK7TNtoX12ac-XXZg33ARbMSnZFJaozKxXKg6jrbks2O1NuFOGYPTDs0g0l0asVzEhuJLh7aYGPxZZejS1B5fmSlo_8CWH8Siri5c8dy4kn0yZPYly-oIw4lNS2LA=s0-d-e1-ft#https://docs.google.com/uc?export=download&amp;id=1O94ewGHQ6a9Ys8n9oZvgDoaEBUEOdAKx&amp;revid=0B5Six9hxnFnSWmtZUGFXQWpxZFUyS0wxdjlpci9IWEcveE9NPQ" width="200" height="68" class="CToWUd">
+                        """
+            elif 40 <= challenge.score < 70:
+                result = 'Need review'
+                html_content = f"""<h3 style="font-weight:normal">Good job.</h3>
+                         <h3 style="font-weight:normal">You will be contacted by Propulsion with your results soon</h3>
+                         <p><strong>Propulsion Academy</strong><br>
+                         Technoparkstrasse 1<br>
+                         8005 Zürich, Switzerland<br>
+                         https://propulsion.academy/full-stack</p>
+                         <img src="https://ci6.googleusercontent.com/proxy/D1srIpj53axfX_D5ZAZRlbc5aW_wo_qIcq9U0HynZroJDhCh-sS_cobQ8ulokzLaAm29-KHvII6JPVqy3tkJueK7TNtoX12ac-XXZg33ARbMSnZFJaozKxXKg6jrbks2O1NuFOGYPTDs0g0l0asVzEhuJLh7aYGPxZZejS1B5fmSlo_8CWH8Siri5c8dy4kn0yZPYly-oIw4lNS2LA=s0-d-e1-ft#https://docs.google.com/uc?export=download&amp;id=1O94ewGHQ6a9Ys8n9oZvgDoaEBUEOdAKx&amp;revid=0B5Six9hxnFnSWmtZUGFXQWpxZFUyS0wxdjlpci9IWEcveE9NPQ" width="200" height="68" class="CToWUd">
+                         """
+            else:
+                result = 'Passed'
+                html_content = f"""<h3 style="font-weight:normal">Great job</h3>
+                        <h3 style="font-weight:normal">You passed the challenge.</h3>
+                        <p><strong>Propulsion Academy</strong><br>
+                        Technoparkstrasse 1<br>
+                        8005 Zürich, Switzerland<br>
+                        https://propulsion.academy/full-stack</p>
+                        <img src="https://ci6.googleusercontent.com/proxy/D1srIpj53axfX_D5ZAZRlbc5aW_wo_qIcq9U0HynZroJDhCh-sS_cobQ8ulokzLaAm29-KHvII6JPVqy3tkJueK7TNtoX12ac-XXZg33ARbMSnZFJaozKxXKg6jrbks2O1NuFOGYPTDs0g0l0asVzEhuJLh7aYGPxZZejS1B5fmSlo_8CWH8Siri5c8dy4kn0yZPYly-oIw4lNS2LA=s0-d-e1-ft#https://docs.google.com/uc?export=download&amp;id=1O94ewGHQ6a9Ys8n9oZvgDoaEBUEOdAKx&amp;revid=0B5Six9hxnFnSWmtZUGFXQWpxZFUyS0wxdjlpci9IWEcveE9NPQ" width="200" height="68" class="CToWUd">
+                        """
+            email.attach_alternative(html_content, "text/html")
+            email.send(fail_silently=False)
+            msg = EmailMultiAlternatives()
+            msg.subject = f'Challenge Results - Candidate: {candidate.first_name} {candidate.last_name}'
+            msg.to = [creator.email]
+            html_content2 = f"""<h3 style="font-weight:normal">Candidate: {candidate.first_name} {candidate.last_name}</h3>
+                                    <h3 style="font-weight:normal">Email: {candidate.email}</h3>
+                                    <h3 style="font-weight:normal">Phone: {candidate.phone}</h3>
+                                    <h3 style="font-weight:normal">Score: {challenge.score}</h3>  
+                                    <h3 style="font-weight:normal">Result: {result}</h3>
+                                    <p><strong>Propulsion Academy</strong><br>
+                                    Technoparkstrasse 1<br>
+                                    8005 Zürich, Switzerland<br>
+                                    https://propulsion.academy/full-stack</p>
+                                    <img src="https://ci6.googleusercontent.com/proxy/D1srIpj53axfX_D5ZAZRlbc5aW_wo_qIcq9U0HynZroJDhCh-sS_cobQ8ulokzLaAm29-KHvII6JPVqy3tkJueK7TNtoX12ac-XXZg33ARbMSnZFJaozKxXKg6jrbks2O1NuFOGYPTDs0g0l0asVzEhuJLh7aYGPxZZejS1B5fmSlo_8CWH8Siri5c8dy4kn0yZPYly-oIw4lNS2LA=s0-d-e1-ft#https://docs.google.com/uc?export=download&amp;id=1O94ewGHQ6a9Ys8n9oZvgDoaEBUEOdAKx&amp;revid=0B5Six9hxnFnSWmtZUGFXQWpxZFUyS0wxdjlpci9IWEcveE9NPQ" width="200" height="68" class="CToWUd">
+                                    """
+            msg.attach_alternative(html_content2, "text/html")
+            msg.send(fail_silently=False)
+
         return Response(status=200)
