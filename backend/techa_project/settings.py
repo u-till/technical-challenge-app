bcb3b57854de7de93388830c2a5f9ab6ffe5a1d1
Natayra Pacheco dos Santos <natayras@gmail.com>
2020-07-24 16:13:37 +0200

    a

--- backend/techa_project/settings.py
+++ backend/techa_project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import ast
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from datetime import timedelta
 
