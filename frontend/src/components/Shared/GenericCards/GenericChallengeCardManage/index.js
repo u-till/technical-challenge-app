52072d65de165a23fe1e3ec342527f5ad1f9db27
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-07-27 11:52:26 +0200

    fix spacing on ManageChallenge

--- frontend/src/components/Shared/GenericCards/GenericChallengeCardManage/index.js
+++ frontend/src/components/Shared/GenericCards/GenericChallengeCardManage/index.js
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
-import { rem } from "polished";
+import React, {useState} from "react";
+import {rem} from "polished";
 import styled from "styled-components";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { BlueButton, RedButton } from "../../../../style/GlobalButtons";
-import { Styledh2 } from "../../../../style/GlobalTitles";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {BlueButton, RedButton} from "../../../../style/GlobalButtons";
+import {Styledh2} from "../../../../style/GlobalTitles";
 import GenericDeleteModal from "../../Modals/GenericDeleteModal/GenericDeleteModal";
 import ReactTooltip from "react-tooltip";
 import {
-  resendChallengeInvitationAction,
-  resendChallengeResultAction,
+    resendChallengeInvitationAction,
+    resendChallengeResultAction,
 } from "../../../../store/actions/challengeActions";
-import { useDispatch } from "react-redux";
+import {useDispatch} from "react-redux";
 
 //////////
 // STYLES
@@ -53,6 +53,12 @@ const Challengeh2 = styled(Styledh2)`
   font-size: ${rem("18px")};
 `;
 
+const ChallengeRightContainer = styled.div`
+  display: flex;
+  width: 70%;
+  justify-content: space-between;
+`;
+
 const DeleteButton = styled(RedButton)`
   padding: 8px;
   width: ${rem("34px")};
@@ -70,98 +76,100 @@ const SendButton = styled(BlueButton)`
 // REACT
 //////////
 
-const GenericChallengeCardManage = ({ challenge }) => {
-  const dispatch = useDispatch();
-  // Used to toggle the delete Challenge Card modal
-  const [isModalDeleteOpen, setModalDeleteOpen] = useState(false);
-  const ModalDeleteOpenCloseHandler = () => {
-    setModalDeleteOpen(!isModalDeleteOpen);
-  };
-  // Used to manage icon displayed on resend buttons during request
-  const [resultStatus, setResultStatus] = useState(false);
-  const [inviteStatus, setInviteStatus] = useState(false);
-  // Used by Resend Invitation Email button
-  const resendInvitationEmail = async (e) => {
-    e.preventDefault();
-    setInviteStatus(true);
-    await dispatch(resendChallengeInvitationAction(challenge.id));
-    setInviteStatus(false);
-  };
-  // Used by Resend Results Email button
-  const resendResultEmail = async (e) => {
-    e.preventDefault();
-    setResultStatus(true);
-    await dispatch(resendChallengeResultAction(challenge.id));
-    setResultStatus(false);
-  };
+const GenericChallengeCardManage = ({challenge}) => {
+    const dispatch = useDispatch();
+    // Used to toggle the delete Challenge Card modal
+    const [isModalDeleteOpen, setModalDeleteOpen] = useState(false);
+    const ModalDeleteOpenCloseHandler = () => {
+        setModalDeleteOpen(!isModalDeleteOpen);
+    };
+    // Used to manage icon displayed on resend buttons during request
+    const [resultStatus, setResultStatus] = useState(false);
+    const [inviteStatus, setInviteStatus] = useState(false);
+    // Used by Resend Invitation Email button
+    const resendInvitationEmail = async (e) => {
+        e.preventDefault();
+        setInviteStatus(true);
+        await dispatch(resendChallengeInvitationAction(challenge.id));
+        setInviteStatus(false);
+    };
+    // Used by Resend Results Email button
+    const resendResultEmail = async (e) => {
+        e.preventDefault();
+        setResultStatus(true);
+        await dispatch(resendChallengeResultAction(challenge.id));
+        setResultStatus(false);
+    };
 
-  return (
-    <ChallengeCard>
-      <div>
-        <Challengeh2>{`Full Stack - Challenge #${challenge.id}`}</Challengeh2>
-      </div>
-      <div>
-        <ChallengeInfo>
-          <p>
-            Candidate:{" "}
-            {`${challenge.candidate.first_name} ${challenge.candidate.last_name}`}
-          </p>
-          <p>Status: {challenge.status}</p>
-        </ChallengeInfo>
-        <ChallengeInfo>
-          <p>Created: {challenge.created.slice(0, 10)}</p>
-          <p>
-            Created by:{" "}
-            {`${challenge.creator.first_name} ${challenge.creator.last_name}`}
-          </p>
-        </ChallengeInfo>
-        <DeleteButton
-          onClick={ModalDeleteOpenCloseHandler}
-          data-tip="Delete Challenge"
-        >
-          <FontAwesomeIcon icon={["far", "trash-alt"]} />
-          <ReactTooltip place="top" type="dark" effect="solid" />
-        </DeleteButton>
-        {isModalDeleteOpen ? (
-          <GenericDeleteModal
-            ModalDeleteOpenCloseHandler={ModalDeleteOpenCloseHandler}
-            type="challenges"
-            typeId={challenge.id}
-            from="managechallenges"
-          >
-            <p>{`Are you sure you want to delete the Challenge #${challenge.id}`}</p>
-          </GenericDeleteModal>
-        ) : null}
-        {challenge.status === "PASSED" ||
-        challenge.status === "FAILED" ||
-        challenge.status === "NEEDS REVIEW" ? (
-          <SendButton
-            data-tip="Resend Challenge Score Email"
-            onClick={resendResultEmail}
-          >
-            {resultStatus ? (
-              <FontAwesomeIcon icon={["fas", "spinner"]} />
-            ) : (
-              <FontAwesomeIcon icon={["fas", "trophy"]} />
-            )}
-            <ReactTooltip place="top" type="dark" effect="solid" />
-          </SendButton>
-        ) : (
-          <SendButton
-            data-tip="Resend Challenge Invitation Email"
-            onClick={resendInvitationEmail}
-          >
-            {inviteStatus ? (
-              <FontAwesomeIcon icon={["fas", "spinner"]} />
-            ) : (
-              <FontAwesomeIcon icon={["fas", "envelope-open-text"]} />
-            )}
-            <ReactTooltip place="top" type="dark" effect="solid" />
-          </SendButton>
-        )}
-      </div>
-    </ChallengeCard>
-  );
+    return (
+        <ChallengeCard>
+            <div>
+                <Challengeh2>{`Full Stack - Challenge #${challenge.id}`}</Challengeh2>
+            </div>
+            <ChallengeRightContainer>
+                <ChallengeInfo>
+                    <p>
+                        Candidate:{" "}
+                        {`${challenge.candidate.first_name} ${challenge.candidate.last_name}`}
+                    </p>
+                    <p>Status: {challenge.status}</p>
+                </ChallengeInfo>
+                <ChallengeInfo>
+                    <p>Created: {challenge.created.slice(0, 10)}</p>
+                    <p>
+                        Created by:{" "}
+                        {`${challenge.creator.first_name} ${challenge.creator.last_name}`}
+                    </p>
+                </ChallengeInfo>
+              <div>
+              <DeleteButton
+                    onClick={ModalDeleteOpenCloseHandler}
+                    data-tip="Delete Challenge"
+                >
+                    <FontAwesomeIcon icon={["far", "trash-alt"]}/>
+                    <ReactTooltip place="top" type="dark" effect="solid"/>
+                </DeleteButton>
+                {isModalDeleteOpen ? (
+                    <GenericDeleteModal
+                        ModalDeleteOpenCloseHandler={ModalDeleteOpenCloseHandler}
+                        type="challenges"
+                        typeId={challenge.id}
+                        from="managechallenges"
+                    >
+                        <p>{`Are you sure you want to delete the Challenge #${challenge.id}`}</p>
+                    </GenericDeleteModal>
+                ) : null}
+                {challenge.status === "PASSED" ||
+                challenge.status === "FAILED" ||
+                challenge.status === "NEEDS REVIEW" ? (
+                    <SendButton
+                        data-tip="Resend Challenge Score Email"
+                        onClick={resendResultEmail}
+                    >
+                        {resultStatus ? (
+                            <FontAwesomeIcon icon={["fas", "spinner"]}/>
+                        ) : (
+                            <FontAwesomeIcon icon={["fas", "trophy"]}/>
+                        )}
+                        <ReactTooltip place="top" type="dark" effect="solid"/>
+                    </SendButton>
+                ) : (
+                    <SendButton
+                        data-tip="Resend Challenge Invitation Email"
+                        onClick={resendInvitationEmail}
+                    >
+                        {inviteStatus ? (
+                            <FontAwesomeIcon icon={["fas", "spinner"]}/>
+                        ) : (
+                            <FontAwesomeIcon icon={["fas", "envelope-open-text"]}/>
+                        )}
+                        <ReactTooltip place="top" type="dark" effect="solid"/>
+                    </SendButton>
+                )}
+              </div>
+            </ChallengeRightContainer>
+        </ChallengeCard>
+    );
 };
 
 export default GenericChallengeCardManage;
