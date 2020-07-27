7f6dfcdf39b28dcb8fe096a3ad17a6a5371b65f9
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-07-27 14:25:08 +0200

    setup logic on challenge page for displaying Tips

--- backend/question/serializers.py
+++ backend/question/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
 from question.models import Question
+from tip.serializers import TipSerializer
 
 
 class ListQuestionSerializer(serializers.ModelSerializer):
+    fk_tip_question = TipSerializer(
+        read_only=True,
+        many=True
+    )
+
     class Meta:
         model = Question
-        fields = ['id', 'name', 'program', 'instructions', 'difficulty', 'question_creator', 'tests_for_question']
+        fields = ['id', 'name', 'program', 'instructions', 'difficulty', 'question_creator', 'tests_for_question',
+                  'fk_tip_question']
 
 
 class CreateQuestionSerializer(serializers.ModelSerializer):
